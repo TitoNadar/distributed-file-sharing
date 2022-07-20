62d7e575601da87bf3818b920ac9ec9b378ac2f9
TitoNadar <titonadar06@gmail.com>
2022-07-20 15:22:50 +0530

    Updates

---
+++ precache-manifest.aa7f173435d3dcd008f9a67ab65941d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d0f7b99a7ffa7fbb1b",
+    "url": "/distributed-file-sharing/static/css/main.b3276bfe.chunk.css"
+  },
+  {
+    "revision": "f9d0f7b99a7ffa7fbb1b",
+    "url": "/distributed-file-sharing/static/js/main.f9d0f7b9.chunk.js"
+  },
+  {
+    "revision": "6ca31e0c5da08fd7873c",
+    "url": "/distributed-file-sharing/static/css/1.8ad77007.chunk.css"
+  },
+  {
+    "revision": "6ca31e0c5da08fd7873c",
+    "url": "/distributed-file-sharing/static/js/1.6ca31e0c.chunk.js"
+  },
+  {
+    "revision": "cfb7239b6f25e802d325",
+    "url": "/distributed-file-sharing/static/js/runtime~main.cfb7239b.js"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/distributed-file-sharing/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/distributed-file-sharing/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "ad56828d94c476fd2c61ca6ba3445216",
+    "url": "/distributed-file-sharing/index.html"
+  }
+];
